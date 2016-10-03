db3bf96f700d3fdc152b3aa8aea146b78494ae99
psykoterro <psykoterro@gmail.com>
2016-10-03 15:27:03 +0200

    upload du site

---
+++ js/custom.js
@@ -0,0 +1,215 @@
+/* ========================================================================= */
+/*	Preloader
+/* ========================================================================= */
+
+jQuery(window).load(function(){
+
+	$("#preloader").fadeOut("slow");
+
+});
+
+
+$(document).ready(function(){
+
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
+
+	jQuery('#nav').singlePageNav({
+		offset: jQuery('#nav').outerHeight(),
+		filter: ':not(.external)',
+		speed: 1200,
+		currentClass: 'current',
+		easing: 'easeInOutExpo',
+		updateHash: true,
+		beforeStart: function() {
+			console.log('begin scrolling');
+		},
+		onComplete: function() {
+			console.log('done scrolling');
+		}
+	});
+	
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400) {
+            $("#navigation").css("background-color","#E10945");
+        } else {
+            $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
+        }
+    });
+	
+	/* ========================================================================= */
+	/*	Fix Slider Height
+	/* ========================================================================= */	
+
+	var slideHeight = $(window).height();
+	
+	$('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#slider, .carousel.slide, .carousel-inner, .carousel-inner .item').css('height',slideHeight);
+	});
+	
+	
+	/* ========================================================================= */
+	/*	Portfolio Filtering
+	/* ========================================================================= */	
+	
+	
+    // portfolio filtering
+
+    $(".project-wrapper").mixItUp();
+	
+	
+	$(".fancybox").fancybox({
+		padding: 0,
+
+		openEffect : 'elastic',
+		openSpeed  : 650,
+
+		closeEffect : 'elastic',
+		closeSpeed  : 550,
+
+		closeClick : true,
+	});
+	
+	/* ========================================================================= */
+	/*	Parallax
+	/* ========================================================================= */	
+	
+	$('#facts').parallax("50%", 0.3);
+	
+	/* ========================================================================= */
+	/*	Timer count
+	/* ========================================================================= */
+
+	"use strict";
+    $(".number-counters").appear(function () {
+        $(".number-counters [data-to]").each(function () {
+            var e = $(this).attr("data-to");
+            $(this).delay(6e3).countTo({
+                from: 50,
+                to: e,
+                speed: 3e3,
+                refreshInterval: 50
+            })
+        })
+    });
+	
+	/* ========================================================================= */
+	/*	Back to Top
+	/* ========================================================================= */
+	
+	
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400) {
+            $("#back-top").fadeIn(200)
+        } else {
+            $("#back-top").fadeOut(200)
+        }
+    });
+    $("#back-top").click(function () {
+        $("html, body").stop().animate({
+            scrollTop: 0
+        }, 1500, "easeInOutExpo")
+    });
+	
+});
+
+
+// ======================================= //
+// ==========  START GOOGLE MAP ========== //
+function initialize() {
+	// modification latitude, longitude //
+    var myLatLng = new google.maps.LatLng(44.1318616, 4.069071799999961);
+
+    var mapOptions = {
+    	// modification du zoom par défaut //
+        zoom: 16,
+        center: myLatLng,
+        disableDefaultUI: true,
+        // je passe scrollwheel à true pour autoriser le zoom sur la carte //
+        scrollwheel: true,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        // Je passe draggable à true afin de rendre la carte plus interractive //
+        draggable: true,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+        }
+    };
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: 'img/location-icon.png',
+        title: '',
+    });
+
+}
+
+google.maps.event.addDomListener(window, "load", initialize);
+// ========== END GOOGLE MAP ========== //
+
+// ========================================================================= //
+//	========================= Contact Form ================================= //
+// ========================================================================= //
+
+$(function(){
+
+				$('#contact-form').validate({
+					rules: {
+						name: {
+							required: true,
+							minlength: 2
+						},
+						email: {
+							required: true,
+							email: true
+						},
+						message: {
+							required: true
+						}
+					},
+					// Modification des dialogues //
+					messages: {
+						name: {
+							required: "Merci d'indiquer votre nom dans ce champ",
+							minlength: "Votre nom n'a pas la longueur requise"
+						},
+						email: {
+							required: "Votre adresse courriel est indispensable"
+						},
+						message: {
+							required: "Quel est votre message",
+							minlength: "Vous n'avez pas de message?"
+						}
+					},
+					submitHandler: function(form) {
+						$(form).ajaxSubmit({
+							type:"POST",
+							data: $(form).serialize(),
+							url:"./php/contact.php",
+							success: function() {
+								$('#contact-form :input').attr('disabled', 'disabled');
+								$('#contact-form').fadeTo( "slow", 0.15, function() {
+									$(this).find(':input').attr('disabled', 'disabled');
+									$(this).find('label').css('cursor','default');
+									$('#success').fadeIn();
+								});
+							},
+							error: function() {
+								$('#contact-form').fadeTo( "slow", 0.15, function() {
+									$('#error').fadeIn();
+								});
+							}
+						});
+					}
+				});
+			});
+
+// ============================Fin de Contact Form ======================= //
